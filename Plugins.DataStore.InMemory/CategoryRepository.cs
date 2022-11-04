9b22a78a8caa7fcbde880be7ff7450ba92e4ea87
Yue Chang <yue.chang@ranplanwireless.com>
2022-11-04 23:26:47 +0000

    Add project files.

---
+++ Plugins.DataStore.InMemory/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> _categories;
+        public CategoryInMemoryRepository()
+        {
+            _categories = new List<Category>()
+            {
+                new Category(){ Id = 1, Name = "Beverage", Description = "A good beverage"} ,
+                new Category(){ Id = 2, Name = "Bakery", Description = "A good bakery"},
+                new Category(){ Id = 3, Name = "Meat", Description = "A good meat"}
+            };
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return _categories;
+        }
+    }
+}
---
+++ UseCases/UserCaseInterfaces/IViewCategoriesUseCase.cs
@@ -0,0 +1,9 @@
+﻿using CoreBusiness;
+
+namespace UseCases.UserCaseInterfaces
+{
+    public interface IViewCategoriesUseCase
+    {
+        IEnumerable<Category> Execute();
+    }
+}
